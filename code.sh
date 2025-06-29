447b0514b13fdd540542a253f62e3b4db83ee066
vinitha <vinithavijayakumar2403@gmail.com>
2025-06-29 08:23:19 +0000

    This is updated file

---
+++ code.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo " checking the list of installed packages"
+dpkg -l apache2 &>/dev/null
+
+if [$? -eq 0];then
+	echo " Apache is already installed"
+else
+	echo " Apache is not installed "
+
+#Attempting second installation
+echo " Attempting to install Apache now "
+sudo apt-get update &>/dev/null
+sudo apt-get install apache2 -y &>/dev/null
+if [$? -eq 0];then
+	echo "Apache has been successfully installed "
+else
+	echo " Apache is not installed"
+      fi
+
+fi
+
+
